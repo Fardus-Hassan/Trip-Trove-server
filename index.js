76e80f2be733bb357c318af28f31d0770f7bd463
Fardus-Hassan <webdev.fardus@gmail.com>
2024-04-30 00:40:08 +0600

    get user email to filter data in my list page

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASS}@cluster0.ak5lvkp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,10 +23,66 @@ const client = new MongoClient(uri, {
     }
 });
 
+
 async function run() {
+
+    const TouristsSpotsCollection = client.db("TouristsDB").collection("TouristsSportsCollection");
+
+
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+
+        app.get('/touristSpots', async (req, res) => {
+            const cursor = TouristsSpotsCollection.find()
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.get('/touristSpots/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await TouristsSpotsCollection.findOne(query)
+            res.send(result)
+        })
+
+        app.get('/touristSpots/myList/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { email : email }
+            const cursor = TouristsSpotsCollection.find(query)
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+        app.post('/touristSpots', async (req, res) => {
+            const touristsData = req.body
+            const result = await TouristsSpotsCollection.insertOne(touristsData)
+            res.send(result)
+        })
+
+        app.delete('/touristSpots/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await TouristsSpotsCollection.deleteOne(query)
+            res.send(result)
+        })
+
+        app.put('/touristSpots/:id', async (req, res) => {
+            const id = req.params.id
+            const touristsData = req.body
+            const query = { _id: new ObjectId(id) }
+            const optional = { upsert: true }
+            const data = {
+                $set: touristsData
+            }
+            const result = await TouristsSpotsCollection.updateOne(query, data, optional)
+            res.send(result)
+        })
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
